ff2c058a6f981deb9cbe3b0984f15d1ccd8b9946
Nitin Bhandari <nsb007@users.noreply.github.com>
2018-01-04 21:13:31 -0600

    Updating FourSquareAPI and adding file with restaurant's info

--- FourSquareAPI.py
+++ FourSquareAPI.py
@@ -1,18 +1,24 @@
+import urllib2
 import json, requests
-#import pyfoursquare as fs
+import io
 
 url = 'https://api.foursquare.com/v2/venues/search?'
 
-params = dict(
-  categoryId='4d4b7105d754a06374d81259',
-  client_id='OXOP1HNDEV0NQOXJ3R5RC5GZWSKIFXOJOGA1KDHZXMNNFEJL',
-  client_secret='1OCNH4GAHSM1OIVYZHISHRRVPM0BSF0OP2PYDQXTCSG4KMLD',
-  near='Kansas+City',
-  v='20170801',
-  limit=100
-)
+res_places = ["Kansas City", "Wichita", "Topeka", "Overland Park", "Lawrence", "Olathe", "Lenexa", "Shawnee"]
 
-resp = requests.get(url=url, params=params)
-data = json.loads(resp.text)
-data = json.dumps(data)
-print data
+for places in res_places:
+	
+	params = dict(
+  	categoryId='54135bf5e4b08f3d2429dfe1',
+  	client_id='OXOP1HNDEV0NQOXJ3R5RC5GZWSKIFXOJOGA1KDHZXMNNFEJL',
+  	client_secret='1OCNH4GAHSM1OIVYZHISHRRVPM0BSF0OP2PYDQXTCSG4KMLD',
+  	near=places,
+  	v='20170801',
+  	limit=50
+	)
+	resp = requests.get(url=url, params=params)
+	data = json.loads(resp.text)
+	print data
+	with io.open('resturant_data.json', 'a+') as outfile:
+		outfile.write(resp.text) 
+	
