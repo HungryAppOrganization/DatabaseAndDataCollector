3f08c29e9c4f8bf28dd8e80a4161ee8dc4f131d3
Nitin Bhandari <nsb007@users.noreply.github.com>
2018-01-06 20:22:12 -0600

    Added Menu functionality
    Menu is being fetched from FourSquareAPI.

--- FourSquareAPI.py
+++ FourSquareAPI.py
@@ -2,7 +2,7 @@
 #  # ### #
 #  ### # #
 #      # #
-#      ###      Hungry LLC 
+#      ###        Hungry LLC 
 #                 Drafted 1/4/2018.
 #      ###        Nitin Bhandari
 
@@ -26,7 +26,7 @@ CLIENT_SEC = '1OCNH4GAHSM1OIVYZHISHRRVPM0BSF0OP2PYDQXTCSG4KMLD'
 ############# Support Functions #############
 #############################################
 
-#This returns in a list all the ids of restaurants in a city. 
+#This returns in a list all the ids, names, contact, address of restaurants in a city. 
 def getAllRestIdsInCity(areasToConsider,lim=1):
 	search_url = 'https://api.foursquare.com/v2/venues/search?'
 	retVal = []
@@ -45,16 +45,46 @@ def getAllRestIdsInCity(areasToConsider,lim=1):
 	    print data
 	    print("Option...")
 	    venList = data['response']['venues']
+	    
 	    retVal = []
+	    names = [] 
+	    contact = []
+	    loc = []
+	    latlong =[]
+
 	    for item in venList:
 	    	retVal.append(item['id'])
+	    	names.append(item['name'])
+	    	contact.append(item['contact']['formattedPhone'])
+	    	loc.append(item['location']['formattedAddress'])
+	    	latlong.append(str(item['location']['lat']) + ',' + str(item['location']['lng']))
 	    #Now each of the ID's are contained in the list here under id
 	    #Eg: print data['response']['venues'][0]['id']
 	    with io.open('resturant_data.json', 'a+') as outfile:
-	        outfile.write(resp.text +"\n") 
+	        outfile.write(resp.text +"\n")
 
-	return retVal
-  
+		return retVal,names,contact,loc,latlong
+
+#This returns menu of the resturants
+
+def getResMenu(res_ID):
+
+	res_Menu = []
+	for ID in res_ID:
+		menu_url = 'https://api.foursquare.com/v2/venues/'+res_ID+'/menu'
+		params = dict(
+	      categoryId=FOOD_CAT,
+	      client_id=CLIENT_ID,
+	      client_secret=CLIENT_SEC,
+	      v='20170801'
+	    )
+		resp = requests.get(url=menu_url, params=params)
+		data = json.loads(resp.text)
+		menuList = data['response']['menu']['menus']
+		res_Menu.append(menuList)
+	return res_Menu
+
+
 #############################################
 ############# Test Functions    #############
 #############################################
@@ -62,16 +92,20 @@ def getAllRestIdsInCity(areasToConsider,lim=1):
 
 res_places = ["Kansas City"]#, "Wichita", "Topeka", "Overland Park", "Lawrence", "Olathe", "Lenexa", "Shawnee"
 #This gets all the IDs.
-resIds = getAllRestIdsInCity(res_places)
+resIds, resNames, resContact, resAddress, resLatLong = getAllRestIdsInCity(res_places)
 print("My venue ids I can use: ")
-print(resIds)
+for i in resIds:
+	print(i)
+	resMenu = getResMenu(i)
+	print resMenu
+
 #Now use these URLS:
-#rest_url = 'https://api.foursquare.com/v2/venues/'4adb44c6f964a520672521e3'
-#menu_url = 'https://api.foursquare.com/v2/venues/'4adb44c6f964a520672521e3/menu'
+#rest_url = 'https://api.foursquare.com/v2/venues/'4adb44c6f964a520672521e3'           								DONE
+#menu_url = 'https://api.foursquare.com/v2/venues/'4adb44c6f964a520672521e3/menu'	   								DONE
 
-#Use these to get the data for the restaurant as well as the menu data, and ordering information (if possible). 
+#Use these to get the data for the restaurant as well as the menu data, and ordering information (if possible). 	DONE
 
-#Then store all this associated data. 
+#Then store all this associated data. 																				????
 
 
 
